74617c43c4086f37a21bc91b4f9b56c40898d1e8
carlsantunes <antunes.carlos98@gmail.com>
2026-04-23 14:52:50 +0000

    Make spark work for databricks and locally

--- conftest.py
+++ conftest.py
@@ -1,15 +1,20 @@
 import pytest
 from pyspark.sql import SparkSession
+import os
 
 @pytest.fixture(scope="session")
 def spark():
-    spark = (
-        SparkSession
-        .builder
-        .master("local[1]")
-        .appName("pytest-pyspark")
-        .getOrCreate()
-    )
+    
+    if os.environ.get("DATABRICKS_RUNTIME_VERSION"):
+        spark = spark  # provided by Databricks
+    else:
+        spark = (
+            SparkSession
+            .builder
+            .master("local[1]")
+            .appName("pytest-pyspark")
+            .getOrCreate()
+        )
 
     yield spark
     spark.stop()
