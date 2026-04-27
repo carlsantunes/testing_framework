10510195ab047fd8ba3dec8cd95c3142bf22290e
Carlos Antunes <cvantunes@ext.worten.pt>
2026-04-27 20:48:15 +0100

    Generate notebook content function

---
+++ src/generate.py
@@ -0,0 +1,27 @@
+import src.notebook.sections as s
+from src.notebook.renderers import render_notebook
+from src.notebook.config.paths import NotebookPaths
+from src.notebook.generator.generator import NotebookGenerator
+
+def generate_notebook_content(nb):
+    paths = NotebookPaths()
+    generator = NotebookGenerator([
+        s.HeaderSection(),
+        s.UtilitiesSection(paths),
+        s.FunctionDeclarationSection(),
+        s.WidgetsSection(),
+        s.PathsSection(),
+        s.StartControlSection(),
+        s.TableCreationSection(),
+        s.TableTagsSection(),
+        s.DependenciesSection(),
+        s.ReadSourcesSection(),
+        s.BusinessLogicSection(),
+        s.ExitIfEmptySection(),
+        s.EncryptionSection(),
+        s.StagingSection(staging_schema=paths.staging_schema),
+        s.MergeDWSection(staging_schema=paths.staging_schema),
+        s.ViewCreationSection(),
+        s.EndControlSection(staging_schema=paths.staging_schema),
+    ])
+    return generator.generate(nb)
